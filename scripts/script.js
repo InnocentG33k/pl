57c90bcade9646b7506b695934b3bd9c19927f3c
innocentgeek <innocentgeek@pm.me>
2022-01-27 15:16:00 +0600

    testing git

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+if ($(window).width() <= 768) {
+$(function() {
+  $("#nav-btn").click(function(){
+    $("#nav-list").slideToggle(200);
+  });
+  $(".nav-link").click(function(){
+    $("#nav-list").toggle();
+  });
+});
+}
+$(window).resize(function() {
+  if ($(this).width() >= 769) {
+    $("#nav-list").show();
+    $("#nav-list").css("display","flex")
+  }
+  if($(window).width() <= 768) {
+    $("#nav-list").hide();
+  }
+});
